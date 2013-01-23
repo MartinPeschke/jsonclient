ddcd8e9fefc4f848ff3857a574141380e0386b88
Martin Peschke <martin@per-4.com>
2013-01-23 21:48:06 +0100

    version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name = 'jsonclientHGMMP',
-    version = '0.0.3',
+    version = '0.0.4',
     description = "Mapping JSON to objects and vice versa, this is a product of Christopher Lenz and Matthew Good with slight modifications",
     long_description = '',
     author = 'Martin Peschke',
