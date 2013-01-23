b82188c51037f20f1e1a400ec34c283103ac4802
Martin Peschke <martin@per-4.com>
2013-01-23 21:47:07 +0100

    initial scaffold copy from hgmmpfork, as it was unwieldy and should not be a formlibrary

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011 Matthew Good
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+# most of the code from Matt Good, https://github.com/mgood/jsonmapper
+
+
+from setuptools import setup
+
+
+setup(
+    name = 'jsonclientHGMMP',
+    version = '0.0.3',
+    description = "Mapping JSON to objects and vice versa, this is a product of Christopher Lenz and Matthew Good with slight modifications",
+    long_description = '',
+    author = 'Martin Peschke',
+    author_email = 'martin@per-4.com',
+    install_requires=['simplejson', 'httplib2'],
+    license = 'BSD',
+    zip_safe = False,
+    url = 'https://github.com',
+    packages = ['jsonclient']
+)
